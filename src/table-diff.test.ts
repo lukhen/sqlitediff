f8ca4bb2eac207682e82162c4fe755b6bfa76ff9
lukhen <lukasz.hen@gmail.com>
2022-04-15 12:33:02 +0200

    We can get at least one columnt from a table in a database.

--- src/table-diff.test.ts
+++ src/table-diff.test.ts
@@ -2,17 +2,36 @@ import * as s from "better-sqlite3"
 import * as E from "fp-ts/lib/Either"
 import * as ts from "io-ts";
 import * as _ from "lodash"
+import { getQueryResult } from "./functions"
+
+const columnCodec = ts.type({
+    name: ts.string
+})
+
+const columnsCodec = ts.array(columnCodec)
+
+interface Column {
+    name: string
+}
 
 const getColumns:
-    (db: s.Database, tableName: string) => E.Either<ts.Errors, string[]> =
-    (db, tableName) => E.left([{ value: "", context: [], message: "no such table" }])
+    (tableName: string) => (db: s.Database) => E.Either<ts.Errors, Column[]> =
+    tableName => getQueryResult<Column>("SELECT name FROM PRAGMA_TABLE_INFO('table1')")(columnsCodec)
 
 
 describe("", () => {
     test("no such table", () => {
         const db1 = new s.default(":memory:")
-        const cols = getColumns(db1, "table1")
-        expect(cols).toEqual(E.left([{ value: "", context: [], message: "no such table" }]))
+        const cols = getColumns("table1")(db1)
+        expect(cols).toEqual(E.right([]))
     })
+
+    test("one column", () => {
+        const db1 = new s.default(":memory:")
+        db1.prepare("CREATE TABLE table1 (id INTEGER PRIMARY KEY)").run()
+        const cols = getColumns("table1")(db1)
+        expect(cols).toEqual(E.right([{ name: "id" }]))
+    })
+
 
 })
