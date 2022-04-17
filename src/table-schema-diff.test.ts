533164f67d709529b5dbc26f33c34c0271c3147d
lukhen <lukasz.hen@gmail.com>
2022-04-17 09:10:02 +0200

    We can produce diff for tables with single different column.

--- src/table-schema-diff.test.ts
+++ src/table-schema-diff.test.ts
@@ -4,6 +4,8 @@ import { getColumns } from "./quries/tableColumns"
 import { failTest } from "./functions"
 import { pipe } from "fp-ts/lib/function"
 import * as ts from "io-ts"
+import * as _ from "lodash"
+import * as A from "fp-ts/lib/Array"
 
 interface TableSchemaDiff {
     intersection: string[],
@@ -19,9 +21,13 @@ const sqliteTableSchemaDiff:
             E.apS("db1Cols", getColumns(tableName)(db1)),
             E.apS("db2Cols", getColumns(tableName)(db2)),
             E.map(({ db1Cols, db2Cols }) => ({
+                db1ColNames: pipe(db1Cols, A.map(col => col.name)),
+                db2ColNames: pipe(db2Cols, A.map(col => col.name))
+            })),
+            E.map(({ db1ColNames, db2ColNames }) => ({
                 intersection: [],
-                db1_db2: [],
-                db2_db1: []
+                db1_db2: _.difference(db1ColNames, db2ColNames),
+                db2_db1: _.difference(db2ColNames, db1ColNames)
             }))
         )
     }
@@ -93,15 +99,36 @@ describe("sqliteTableSchemaDiff", () => {
                 errors => {
                     failTest("this should not be reached")()
                 },
-                diff => ({
-
+                diff => (expect(diff).toEqual({
                     intersection: [],
                     db1_db2: [],
                     db2_db1: []
 
-                })
+                }))
             )
         )
     })
 
+    test("both tables have each single different columns", () => {
+        const db1 = new s.default(":memory:")
+        db1.prepare("CREATE TABLE table1 (col1 INTEGER PRIMARY KEY)").run()
+        const db2 = new s.default(":memory:")
+        db2.prepare("CREATE TABLE table1 (col2 INTEGER PRIMARY KEY)").run()
+        const diff = sqliteTableSchemaDiff("table1", db1, db2)
+        pipe(
+            diff,
+            E.fold(
+                errors => {
+                    failTest("this should not be reached")()
+                },
+                diff => (expect(diff).toEqual({
+
+                    intersection: [],
+                    db1_db2: ["col1"],
+                    db2_db1: ["col2"]
+
+                }))
+            )
+        )
+    })
 })
