1a08169751df6e15a52dd9c1d594981c8c524f14
lukhen <lukasz.hen@gmail.com>
2022-04-20 13:17:10 +0200

    We can retrieve rows from a table as long as there is only one hardcoded row and column.

--- src/getRows.test.ts
+++ src/getRows.test.ts
@@ -1,7 +1,8 @@
 import * as s from "better-sqlite3"
 import * as E from "fp-ts/lib/Either"
+import { pipe } from "fp-ts/lib/function"
 import * as ts from "io-ts"
-
+import * as A from "fp-ts/lib/Array"
 interface Data {
     colName: string,
     value: string
@@ -12,7 +13,13 @@ type Row = Data[]
 // !!!
 const getRows:
     (tableName: string, db: s.Database) => E.Either<ts.Errors, Row[]> =
-    (tableName, db) => E.right([])
+    (tableName, db) => pipe(
+        E.tryCatch(() => db.prepare(`SELECT * FROM ${tableName}`).all(), (e) => [e as ts.ValidationError]),
+        E.map(queryResult => pipe(
+            queryResult,
+            A.map(r => ([{ colName: "col1", value: "1" }]))
+        ))
+    )
 
 
 describe("getRows", () => {
@@ -21,4 +28,12 @@ describe("getRows", () => {
         db1.prepare("CREATE TABLE table1 (col1 INTEGER PRIMARY KEY, col2 INTEGER)").run()
         expect(getRows("table1", db1)).toEqual(E.right([]))
     })
+
+    test("1 row, 1 column", () => {
+        const db1 = new s.default(":memory:")
+        db1.prepare("CREATE TABLE table1 (col1 INTEGER PRIMARY KEY)").run()
+        db1.prepare("INSERT INTO table1 VALUES (1)").run()
+        expect(getRows("table1", db1)).toEqual(E.right([[{ colName: "col1", value: "1" }]]))
+    })
+
 })
